a298eaacf28628d783caf2079bd97744424fe950
MHalseyPerry <milo.halseyperry@gmail.com>
2019-01-07 16:53:50 +0000

    Began adding ability to add tasks to specific projects

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -6,11 +6,20 @@
 
         <h1>{{$project->title}}</h1>
 
-        <div class="content">{{ $project->description }}</div>
+        <div class="content">{{ $project->description }}
+            <p>
+                <a href="{{route('projects.edit', $project)}}">Edit/Delete</a></td>
+            </p>
+        </div>
 
-        <p>
-            <a href="{{route('projects.edit', $project)}}">Edit/Delete</a></td>
-        </p>
+        @if ($project->tasks->count())
+            <div>
+                @foreach ($project->tasks as $task)
+                    <li> {{ $task->description }} </li>
+                @endforeach
+            </div>
+        @endif
+
 
 
 @endsection
